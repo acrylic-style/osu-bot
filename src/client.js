b64d024a51acb00a420366b68b2c787e70ade538
acrylic-style <me@acrylicstyle.xyz>
2019-04-12 21:14:26 +0900

    now shows more information.

--- src/client.js
+++ src/client.js
@@ -15,12 +15,17 @@ client.on('presenceUpdate', (og, nm) => {
   if (game.applicationID !== '367827983903490050') return // <- return if application id isn't osu client
   if (!/^(.*?) - (.*) \[(.*)\]$/g.test(game.details)) return
   const [ beatmap, artist, name, difficulty ] = /^(.*?) - (.*) \[(.*)\]$/g.exec(game.details) // (Artist) - (Beatmap) [(Difficulty)]
+  const [ namerank, osuname, rank ] = /(.*?) \(rank (.*)\)/.exec(game.assets.largeText) // (osu Username) - \(rank (#Rank)\)
   const embed = new Discord.RichEmbed()
-    .setTitle(`${nm.user.tag} is ${game.state}`)
-    .setURL(`https://osu.ppy.sh/beatmapsets?q=${encodeURI(beatmap)}&s=7`)
+    .setTitle(`${osuname} (${nm.user.tag}) is ${game.state.toLowerCase()}`)
+    .setURL(`https://acrylicstyle.xyz/osu/spectate/${encodeURI(osuname)}`)
+    .addField('Beatmap URL', `https://osu.ppy.sh/beatmapsets?q=${encodeURI(beatmap)}&s=7`)
+    .addField('User Profile', `https://osu.ppy.sh/u/${osuname}`)
     .addField('Artist', artist)
     .addField('Beatmap Name', name)
     .addField('Difficulty Name', difficulty)
+    .addField('Current player rank', rank)
+    .setColor([0,255,0])
   if (game.state === 'Spectating') {
     !nm.guild.channels.find(c => c.name === 'now-spectating') || nm.guild.channels.find(c => c.name === 'now-spectating').send(embed)
   } else {
